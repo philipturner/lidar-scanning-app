e8c75c07bb47e18fa41d45bcd3f65676b3de08fd
Philip Turner <philipturner.AR@gmail.com>
2022-11-08 10:14:28 -0500

    Save progress

--- LiDARScanningApp/Coordinator.swift
+++ LiDARScanningApp/Coordinator.swift
@@ -7,12 +7,50 @@
 
 import SwiftUI
 import MetalKit
+import ARKit
 
 class Coordinator: NSObject, ObservableObject {
-  var view: MTKView!
+  var session: ARSession
+  var view: MTKView
+//  var renderer: MainRenderer!
   
   override init() {
-    print("Hello world, this succeeded.")
+    self.session = ARSession()
+    
+    guard ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) else {
+      fatalError("This iOS device does not have LiDAR.")
+    }
+    
+    let configuration = ARWorldTrackingConfiguration()
+    configuration.sceneReconstruction = .mesh
+    configuration.frameSemantics.insert(.sceneDepth)
+    configuration.frameSemantics.insert(.personSegmentation)
+    
+    if configuration.videoFormat.imageResolution != CGSize(width: 1920, height: 1440) {
+      let formats = ARWorldTrackingConfiguration.supportedVideoFormats
+      if let desiredFormat = formats.first(where: {
+        $0.imageResolution == CGSize(width: 1920, height: 1440)
+      }) {
+        configuration.videoFormat = desiredFormat
+      } else {
+        fatalError("This device lacks 1920 x 1440 image resolution.")
+      }
+    }
+    session.run(configuration)
+    
+    self.view = MTKView()
+    
+    let nativeBounds = UIScreen.main.nativeBounds
+    view.drawableSize = .init(width: nativeBounds.height, height: nativeBounds.width)
+    view.autoResizeDrawable = false
+    
+    let castedLayer = view.layer as! CAMetalLayer
+    castedLayer.framebufferOnly = false
+    castedLayer.allowsNextDrawableTimeout = false
+    
+    view.device = MTLCreateSystemDefaultDevice()!
+    view.colorPixelFormat = .bgr10_xr
+
     super.init()
   }
 }
