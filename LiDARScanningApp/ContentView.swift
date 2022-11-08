a00f3cd3755b3533103330ca9e6df9af32f53988
Philip Turner <philipturner.AR@gmail.com>
2022-11-08 09:50:19 -0500

    Initial Commit

---
+++ LiDARScanningApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LiDARScanningApp
+//
+//  Created by Philip Turner on 11/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiDARScanningApp/LiDARScanningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiDARScanningAppApp.swift
+//  LiDARScanningApp
+//
+//  Created by Philip Turner on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LiDARScanningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
